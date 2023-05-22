cf45a1c3320268778cdaac9ebffd9a80936fa606
hhike <hhike@outlook.com>
2023-05-21 21:33:55 -0700

    Add new ponya images

--- app/vizdev/VizdevGallery.react.tsx
+++ app/vizdev/VizdevGallery.react.tsx
@@ -5,6 +5,9 @@ import SprKalmanMichelle06PropDesign from "@/public/2019_Spr_Kalman_Michelle_06_
 import SprKalmanMichelle07PropStoryMoment from "@/public/2019_Spr_Kalman_Michelle_07_PropStoryMoment.jpg";
 import SprKalmanMichelle10Environment2 from "@/public/2019_Spr_Kalman_Michelle_10_Environment2.jpg";
 import KalmanMichelleFinalCharacterKyi from "@/public/Kalman_Michelle_Final_CharacterKyi.jpg";
+import KalmanMichelleFinalCharacterMaung from "@/public/Kalman_Michelle_Final_CharacterMaung.jpg";
+import KalmanMichelleFinalCharacterTurnaround from "@/public/Kalman_Michelle_Final_CharacterTurnaround.jpg";
+import KalmanMichelleFinalVehicleBreakaway from "@/public/Kalman_Michelle_Final_VehicleBreakaway.jpg";
 import KalmanMichelleFinalEstablishingShot from "@/public/Kalman_Michelle_Final_EstablishingShot.jpg";
 import KalmanMichelleFinalStoryMoment from "@/public/Kalman_Michelle_Final_StoryMoment.jpg";
 import KalmanMichelleFinalVehicle from "@/public/Kalman_Michelle_Final_Vehicle.jpg";
@@ -21,6 +24,15 @@ export default function VizdevGallery() {
       title: "Ponya",
       images: [
         { src: KalmanMichelleFinalCharacterKyi, title: "Character: Kyi" },
+        { src: KalmanMichelleFinalCharacterMaung, title: "Character: Maung" },
+        {
+          src: KalmanMichelleFinalCharacterTurnaround,
+          title: "Character turnaround",
+        },
+        {
+          src: KalmanMichelleFinalVehicleBreakaway,
+          title: "Vehicle Breakaway",
+        },
         {
           src: KalmanMichelleFinalEstablishingShot,
           title: "Establishing Shot",

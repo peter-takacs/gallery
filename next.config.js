806d4fa009c1780b94a1a35fdf5360f8f66bb768
hhike <hhike@outlook.com>
2023-05-21 20:31:15 -0700

    Remove build config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    loader: "custom",
-    loaderFile: "./app/image.ts",
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
